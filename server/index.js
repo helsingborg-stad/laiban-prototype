f1e16e9e58f9720e52ffeb1c605eea8c525ffc5b
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-04-28 20:34:46 +0200

    Setup nodejs server, add endpoint point for static sources & text2speech service

---
+++ server/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable global-require */
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const { googleText2Speech } = require('./googleText2Speech');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static assets
+app.use(express.static(`${process.cwd()}/public`));
+
+// Endpoints
+app.get('/', (request, response) => {
+    response.sendFile(path.join(`${process.cwd()}/public/index.html`));
+});
+
+app.post('/api/v1/text2Speech', (request, response) => {
+    const { text } = request.body;
+
+    if (typeof text !== 'string' || text.length <= 0) {
+        response.json({ status: 'failed' });
+        return;
+    }
+
+    googleText2Speech(text).then(result => {
+        result.status = 'success';
+        response.json(result);
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server listening on port ', PORT);
+});
