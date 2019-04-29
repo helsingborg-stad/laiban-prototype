377f2c23a791df3a3535f38a27cbfc7d6d0a0429
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-04-29 21:27:03 +0200

    Add weather symbol

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 
+import weatherIcon from '../assets/images/symbols/large-cloud.png';
+
 export default class Weather extends Component {
     render() {
         return (
-            <div className="grid-container">
-                <Grid container spacing={32} />
+            <div className="text-center">
+                <div className="symbol">
+                    <img className="symbol__icon" src={weatherIcon} />
+                </div>
             </div>
         );
     }
