4e2463677fd0464f3045f886b5156b23872acc4f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-04-29 21:26:38 +0200

    Use menu component

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import lunchIcon from '../assets/images/home/home-plate.png';
 import weatherIcon from '../assets/images/home/home-cloud-sun.png';
@@ -8,63 +6,28 @@ import clothingIcon from '../assets/images/home/home-playground.png';
 import clockIcon from '../assets/images/home/home-time.png';
 import dayIcon from '../assets/images/home/home-schedule.png';
 
+import Menu from './shared/Menu.jsx';
+
 export default class Navigation extends Component {
     render() {
         return (
-            <div className="grid-container">
-                <Grid container spacing={32} justify={'center'}>
-                    <Grid item xs={4}>
-                        <Link to="/clothing">
-                            <div className="text-center">
-                                <div className="navigation-icon">
-                                    <img src={clothingIcon} />
-                                </div>
-                                <div className="navigation-content">Vad ska jag klä på mig?</div>
-                            </div>
-                        </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Link to="/lunch">
-                            <div className="text-center">
-                                <div className="navigation-icon">
-                                    <img src={lunchIcon} />
-                                </div>
-                                <div className="navigation-content">Vad blir det för mat?</div>
-                            </div>
-                        </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Link to="/weather">
-                            <div className="text-center">
-                                <div className="navigation-icon">
-                                    <img src={weatherIcon} />
-                                </div>
-                                <div className="navigation-content">Vad blir det för väder?</div>
-                            </div>
-                        </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Link to="/clock">
-                            <div className="text-center">
-                                <div className="navigation-icon">
-                                    <img src={clockIcon} />
-                                </div>
-                                <div className="navigation-content">Vad är klockan?</div>
-                            </div>
-                        </Link>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Link to="/clock">
-                            <div className="text-center">
-                                <div className="navigation-icon">
-                                    <img src={dayIcon} />
-                                </div>
-                                <div className="navigation-content">Vad är det för dag idag?</div>
-                            </div>
-                        </Link>
-                    </Grid>
-                </Grid>
-            </div>
+            <Menu container>
+                <Menu item to={'/clothing'} icon={clothingIcon}>
+                    Vi ska gå ut
+                </Menu>
+                <Menu item to={'/lunch'} icon={lunchIcon}>
+                    Vad blir det för mat idag?
+                </Menu>
+                <Menu item to={'/clock'} icon={clockIcon}>
+                    Vad är klockan?
+                </Menu>
+                <Menu item to={'/calendar'} icon={dayIcon}>
+                    Vad är det för dag idag?
+                </Menu>
+                <Menu item to={'/weather'} icon={weatherIcon}>
+                    Vad är det för väder ute?
+                </Menu>
+            </Menu>
         );
     }
 }
