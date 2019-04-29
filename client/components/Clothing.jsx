d12349d7f6559f81beff162fb4b9056e1c4bdea9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-04-29 16:13:09 +0200

    Import & map images to each garment

--- client/components/Clothing.jsx
+++ client/components/Clothing.jsx
@@ -3,6 +3,19 @@ import Grid from '@material-ui/core/Grid';
 
 import getForecast from '../service/getForecast';
 
+// Images
+import raincoat from '../assets/images/clothes/raincoat.png';
+import rainpants from '../assets/images/clothes/pants-rain.png';
+import rainboots from '../assets/images/clothes/rain-boots.png';
+import winterhat from '../assets/images/clothes/winter-hat.png';
+import winterjacket from '../assets/images/clothes/jacket-1.png';
+import winterpants from '../assets/images/clothes/pants-pullover.png';
+import winterboots from '../assets/images/clothes/winter-boots.png';
+import hat from '../assets/images/clothes/hat.png';
+import sweater from '../assets/images/clothes/sweater-1.png';
+import shoes from '../assets/images/clothes/shoes.png';
+import sunCream from '../assets/images/clothes/sun-cream.png';
+
 class Clothing extends Component {
     state = { clothingSet: '' };
 
@@ -34,6 +47,7 @@ class Clothing extends Component {
                     {clothes.length > 0
                         ? clothes.map(garment => (
                               <Grid item xs={12} key={garment.text}>
+                                  <img src={garment.image} />
                                   {garment.text}
                               </Grid>
                           ))
@@ -45,15 +59,28 @@ class Clothing extends Component {
 }
 
 const CLOTHING_SETS = {
-    rain: [{ text: 'Regnjacka' }, { text: 'Regnbyxor' }, { text: 'Regnstövlar' }],
-    cold: [{ text: 'Mössa' }, { text: 'Jacka' }, { text: 'Överdragsbyxor' }, { text: 'Kängor' }],
-    neutral: [
-        { text: 'Keps' },
-        { text: 'Varm tröja' },
-        { text: 'Skor' },
-        { text: 'Solkräm kanske?' },
+    rain: [
+        { text: 'Regnjacka', image: raincoat },
+        { text: 'Regnbyxor', image: rainpants },
+        { text: 'Regnstövlar', image: rainboots },
     ],
-    hot: [{ text: 'Keps' }, { text: 'Skor' }, { text: 'Solkräm kanske?' }],
+    cold: [
+        { text: 'Mössa', image: winterhat },
+        { text: 'Jacka', image: winterjacket },
+        { text: 'Överdragsbyxor', image: winterpants },
+        { text: 'Kängor', image: winterboots },
+    ],
+    neutral: [
+        { text: 'Keps', image: hat },
+        { text: 'Varm tröja', image: sweater },
+        { text: 'Skor', image: shoes },
+        { text: 'Solkräm kanske?', image: sunCream },
+    ],
+    hot: [
+        { text: 'Keps', image: hat },
+        { text: 'Skor', image: shoes },
+        { text: 'Solkräm kanske?', image: sunCream },
+    ],
 };
 
 export default Clothing;
