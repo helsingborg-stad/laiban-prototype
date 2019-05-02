8d05426673197411d0d07bf13c83c15765792b9f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-02 19:56:34 +0200

    Manuscript component

---
+++ client/components/shared/Manuscript.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Speech from './Speech.jsx';
+import Bubble from './Bubble.jsx';
+
+export default class Manuscript extends Component {
+    state = {
+        queue: this.props.content,
+        audioPiece: '',
+        pieces: [],
+    };
+
+    static propTypes = {
+        content: PropTypes.arrayOf(PropTypes.string),
+        render: PropTypes.func,
+        onEnd: PropTypes.func,
+        disableSpeech: PropTypes.bool,
+    };
+
+    componentDidMount() {
+        this.nextPiece();
+    }
+
+    nextPiece = () => {
+        const { queue } = this.state;
+        const { onEnd } = this.props;
+
+        // Unmount audio
+        this.setState({ audioPiece: '' });
+
+        if (queue.length <= 0) {
+            if (typeof onEnd !== 'undefined') {
+                onEnd();
+            }
+
+            return;
+        }
+
+        this.setState((state, props) => {
+            const { queue, pieces } = state;
+            const currentPiece = queue.shift();
+            pieces.push(currentPiece);
+
+            return {
+                queue: queue,
+                audioPiece:
+                    typeof currentPiece === 'object' && typeof currentPiece.text === 'string'
+                        ? currentPiece.text
+                        : currentPiece,
+                pieces: pieces,
+            };
+        });
+    };
+
+    render() {
+        const { audioPiece, pieces } = this.state;
+        const { render } = this.props;
+        return (
+            <div>
+                {typeof render === 'function' ? render(this.state) : null}
+
+                {audioPiece.length > 0 ? <Speech text={audioPiece} onEnd={this.nextPiece} /> : null}
+            </div>
+        );
+    }
+}
