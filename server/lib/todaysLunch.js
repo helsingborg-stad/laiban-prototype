c17fe52c9743f4258e85a3983d370345e7be95dc
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-03 17:06:23 +0200

    Correction

--- server/lib/todaysLunch.js
+++ server/lib/todaysLunch.js
@@ -5,53 +5,90 @@ const todaysLunch = () => {
         dateFns.isSameDay(new Date(), new Date(date))
     );
 
-    let todaysMenu = '';
+    const todaysMenu = [];
 
-    if (todaysMenuKey.length === 1) {
-        todaysMenu = `Idag blir det ${LUNCH_MENU[todaysMenuKey[0]]}`;
+    if (
+        todaysMenuKey.length === 1 &&
+        Array.isArray(LUNCH_MENU[todaysMenuKey[0]]) &&
+        LUNCH_MENU[todaysMenuKey[0]].length > 0
+    ) {
+        todaysMenu.push('Idag blir det ..');
+
+        LUNCH_MENU[todaysMenuKey[0]].forEach(item => {
+            todaysMenu.push(item);
+        });
     }
 
     if (todaysMenu.length <= 0) {
-        todaysMenu = 'Idag ska köket överraska oss så jag vet inte vad det blir för mat.';
+        todaysMenu.push('Idag ska köket överraska oss så jag vet inte vad det blir för mat.');
     }
 
     return todaysMenu;
 };
 
 const LUNCH_MENU = {
-    '05 02 2019':
-        'Köttbullar med potatis och brunsås. Lingonsylt och grönsaker till det. veg: Morotsbullar med potatis. Kryddig sås, lingonsylt och grönsaker till det.',
-    '05 03 2019':
-        'Korv stroganoff med matvete eller pasta och grönsaker. veg: Soja stroganoff med matvete eller pasta och grönsaker.',
-    '05 06 2019':
-        'Köttbullar med potatis och brunsås. Lingonsylt och grönsaker till det. veg: Morotsbullar med potatis. Kryddig sås, lingonsylt och grönsaker till det.',
-    '05 07 2019':
-        'Korv stroganoff med matvete eller pasta och grönsaker. veg: Soja stroganoff med matvete eller pasta och grönsaker.',
-    '05 08 2019':
-        'Kyckling och potatis med sås och grönsaker. veg: Quornfilé och potatis med sås och grönsaker',
-    '05 09 2019':
-        'Fiskpinnar och potatis med remouladsås och grönsaker.veg: Grönsaksmedaljong och potatis med kryddig sås och grönsaker.',
-    '05 10 2019':
-        'Falafel med Tortillabröd, vitlökssås och grönsaker. veg: Sojakebab med tortillabröd, vitlökssås och grönsaker.',
-    '05 13 2019':
-        'Potatisbullar med blodpudding, skinktärningar och grönsaker. veg: Potatisbullar med facon och grönsaker.',
-    '05 14 2019':
-        'Fiskgratäng med potatismos och grönsaker. veg: grönsaksgratäng med potatismos och grönsaker.',
-    '05 15 2019': 'Gryta med matvete eller risoni och grönsaker.Veg: samma',
-    '05 16 2019': 'Pasta carbonara och grönsaker.Veg: pasta carbonara med linser och grönsaker.',
-    '05 17 2019':
-        'Köttfärslimpa med potatis, sås och grönsaker.veg: Sojafärslimpa med potatis kryddig sås och grönsaker.',
-    '05 27 2019':
-        'Köttbullar med potatis, sås och grönsaker.veg: morotsbullar med potatis. kryddig sås, lingonsylt och grönsaker.',
-    '05 28 2019':
-        'Tikka Masala med matvete eller risoni och grönsaker.veg: Tikka Masala på sojastrimlor med matvete eller risoni och grönsaker.',
-    '05 29 2019':
-        'Fiskpinnar och potatis med remouladsås och grönsaker.veg: Grönsaksmedaljong och potatis med kryddig sås och grönsaker.',
-    '05 31 2019':
+    '05 02 2019': [
+        'Köttbullar med potatis och brunsås. Lingonsylt och grönsaker till det.',
+        'Morotsbullar med potatis. Kryddig sås, lingonsylt och grönsaker till det.',
+    ],
+    '05 03 2019': [
+        'Korv stroganoff med matvete eller pasta och grönsaker.',
+        'Soja stroganoff med matvete eller pasta och grönsaker.',
+    ],
+    '05 06 2019': [
+        'Köttbullar med potatis och brunsås. Lingonsylt och grönsaker till det.',
+        'Morotsbullar med potatis. Kryddig sås, lingonsylt och grönsaker till det.',
+    ],
+    '05 07 2019': [
+        'Korv stroganoff med matvete eller pasta och grönsaker.',
+        'Soja stroganoff med matvete eller pasta och grönsaker.',
+    ],
+    '05 08 2019': [
+        'Kyckling och potatis med sås och grönsaker.',
+        'Quornfilé och potatis med sås och grönsaker',
+    ],
+    '05 09 2019': [
+        'Fiskpinnar och potatis med remouladsås och grönsaker.',
+        'Grönsaksmedaljong och potatis med kryddig sås och grönsaker.',
+    ],
+    '05 10 2019': [
+        'Falafel med Tortillabröd, vitlökssås och grönsaker.',
+        'Sojakebab med tortillabröd, vitlökssås och grönsaker.',
+    ],
+    '05 13 2019': [
+        'Potatisbullar med blodpudding, skinktärningar och grönsaker.',
+        'Potatisbullar med facon och grönsaker.',
+    ],
+    '05 14 2019': [
+        'Fiskgratäng med potatismos och grönsaker.',
+        'Grönsaksgratäng med potatismos och grönsaker.',
+    ],
+    '05 15 2019': ['Gryta med matvete eller risoni och grönsaker.'],
+    '05 16 2019': ['Pasta carbonara och grönsaker.', 'Pasta carbonara med linser och grönsaker.'],
+    '05 17 2019': [
+        'Köttfärslimpa med potatis, sås och grönsaker.',
+        'Sojafärslimpa med potatis kryddig sås och grönsaker.',
+    ],
+    '05 27 2019': [
+        'Köttbullar med potatis, sås och grönsaker.',
+        'Morotsbullar med potatis. kryddig sås, lingonsylt och grönsaker.',
+    ],
+    '05 28 2019': [
+        'Tikka Masala med matvete eller risoni och grönsaker.',
+        'Tikka Masala på sojastrimlor med matvete eller risoni och grönsaker.',
+    ],
+    '05 29 2019': [
+        'Fiskpinnar och potatis med remouladsås och grönsaker.',
+        'Grönsaksmedaljong och potatis med kryddig sås och grönsaker.',
+    ],
+    '05 31 2019': [
         'Finns ingen matsedel för den räknas som lovdag. Förslag: Idag ska köket överraska oss så jag vet inte vad det blir för mat.',
-    '06 03 2019':
-        'Korv med potatismos och grönsaker.Veg: vegoprinskorvar med potatismos och grönsaker.',
-    '06 04 2019': 'Ugnspannkaka och grönsaker.Veg: grönsaksomelett och grönsaker.',
+    ],
+    '06 03 2019': [
+        'Korv med potatismos och grönsaker.',
+        'Vegoprinskorvar med potatismos och grönsaker.',
+    ],
+    '06 04 2019': ['Ugnspannkaka och grönsaker.', 'Grönsaksomelett och grönsaker.'],
 };
 
 module.exports = { todaysLunch };
