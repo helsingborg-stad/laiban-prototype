08debb7cedcbe2fdd50e1efc8bb2211a952e7136
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-06 02:48:16 +0200

    https

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const https = require('https');
 
 const { googleText2Speech } = require('./service/googleText2Speech');
 const { getForecast } = require('./service/getForecast');
@@ -81,6 +82,10 @@ app.listen(PORT, () => {
 // Prevent Heroku from sleeping
 if (typeof process.env.HEROKU_URL !== 'undefined') {
     setInterval(function() {
+        if (process.env.HEROKU_URL.startsWith('https')) {
+            https.get(process.env.HEROKU_URL);
+            return;
+        }
         http.get(process.env.HEROKU_URL);
     }, 300000); // every 5 minutes (300000)
 }
