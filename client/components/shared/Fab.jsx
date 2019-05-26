cb79f7f060eaf59c294a0ee8321cb4db53d3dfa8
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-26 14:08:31 +0200

    Add click callback option

--- client/components/shared/Fab.jsx
+++ client/components/shared/Fab.jsx
@@ -5,10 +5,11 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class Fab extends Component {
     static propTypes = {
-        to: PropTypes.string.isRequired,
+        to: PropTypes.string,
         icon: PropTypes.string,
         color: PropTypes.oneOf(['default', 'danger', 'success']),
         text: PropTypes.string,
+        onClickCallback: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
     };
 
     static defaultProps = {
@@ -16,23 +17,29 @@ export default class Fab extends Component {
     };
 
     render() {
-        const { icon, color, text, to } = this.props;
+        const { icon, color, text, to, onClickCallback } = this.props;
 
         if (typeof icon === 'undefined' && typeof text === 'undefined') {
             return null;
         }
 
+        if (typeof onClickCallback === 'function') {
+            return (
+                <Link
+                    to={to}
+                    onClick={e => {
+                        e.preventDefault();
+                        onClickCallback(e);
+                    }}
+                >
+                    <ButtonContent color={color} text={text} icon={icon} />
+                </Link>
+            );
+        }
+
         return (
             <Link to={to}>
-                <div
-                    className={classNames('fab', {
-                        'fab--extended': typeof text !== 'undefined' && text.length > 0,
-                        'fab--success': color === 'success',
-                        'fab--danger': color === 'danger',
-                    })}
-                >
-                    {text || <FabIcon icon={icon} />}
-                </div>
+                <ButtonContent color={color} text={text} icon={icon} />
             </Link>
         );
     }
@@ -41,5 +48,17 @@ export default class Fab extends Component {
 const FabIcon = props => (
     <div className="fab__icon">
         <img src={props.icon} />
+    </div>
+);
+
+const ButtonContent = props => (
+    <div
+        className={classNames('fab', {
+            'fab--extended': typeof props.text !== 'undefined' && props.text.length > 0,
+            'fab--success': props.color === 'success',
+            'fab--danger': props.color === 'danger',
+        })}
+    >
+        {props.text || <FabIcon icon={props.icon} />}
     </div>
 );
