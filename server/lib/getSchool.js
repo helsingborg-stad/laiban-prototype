34175a6b0c2106ab1d71dfa3d2252a4a75a07aaa
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-26 13:10:36 +0200

    get specific school data by ID

---
+++ server/lib/getSchool.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable prefer-destructuring */
+const fetch = require('node-fetch');
+const dateFns = require('date-fns');
+const sv = require('date-fns/locale/sv');
+
+const { fetchSchools } = require('../service/fetchSchools');
+
+const getSchool = async schoolId => {
+    return fetchSchools().then(schools => {
+        if (!Array.isArray(schools) || schools.length < 1) {
+            throw new Error('No schools in database');
+        }
+
+        const school = schools.filter(school => {
+            return school.id === schoolId;
+        });
+
+        if (school.length !== 1) {
+            throw new Error('School does not exists');
+        }
+
+        return school[0];
+    });
+};
+
+module.exports = { getSchool };
