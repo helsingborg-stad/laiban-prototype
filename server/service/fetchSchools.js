d70902010157944cbd714d7f9c26f6cc015578cb
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-05-29 07:16:18 +0200

    Cache school data & add clearCache method

--- server/service/fetchSchools.js
+++ server/service/fetchSchools.js
@@ -1,4 +1,9 @@
 const fetch = require('node-fetch');
+const fs = require('fs-extra');
+const path = require('path');
+const dateFns = require('date-fns');
+
+const schoolsJson = path.resolve(process.cwd(), 'schools.json');
 
 const fetchSchools = async () => {
     if (typeof process.env.CMS_BASE_URL === 'undefined' || process.env.CMS_BASE_URL.length <= 0) {
@@ -8,16 +13,39 @@ const fetchSchools = async () => {
         };
     }
 
-    const baseUrl = process.env.CMS_BASE_URL;
-    const url = `${baseUrl}/wp-json/wp/v2/preschool`;
+    const cacheExists = await fs.pathExists(schoolsJson);
 
-    return fetch(url)
+    if (cacheExists) {
+        return fs.readJson(schoolsJson).then(async data => {
+            const cacheLifetimeInMinutes = 10;
+
+            if (dateFns.differenceInMinutes(new Date(), data.created) >= cacheLifetimeInMinutes) {
+                await clearCache();
+
+                return fetchSchools();
+            }
+
+            return data.schools;
+        });
+    }
+
+    return fetch(`${process.env.CMS_BASE_URL}/wp-json/wp/v2/preschool`)
         .then(response => {
             return response.json();
         })
         .then(json => {
+            fs.outputJson(schoolsJson, {
+                created: new Date(),
+                schools: json,
+            });
             return json;
         });
 };
 
-module.exports = { fetchSchools };
+const clearCache = async () => {
+    return fs.remove(schoolsJson).catch(err => {
+        console.error(err);
+    });
+};
+
+module.exports = { fetchSchools, clearCache };
